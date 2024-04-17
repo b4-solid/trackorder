dc85c5dbe7e4e3e8dd2842d455f4441d52e48384
aldenluthfi <alden.luthfi@ui.ac.id>
2024-04-17 19:10:04 +0700

    First Commit!

---
+++ src/main/java/id/ac/ui/cs/advpro/trackorder/TrackorderApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advpro.trackorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackorderApplication.class, args);
+	}
+
+}
