2a949b71d127fb5db463b4d02d3ab3ba92c77453
Oey Joshua Jodrian <joshuajodrian@gmail.com>
2024-05-06 23:40:51 +0700

    [REFACTOR]Fix typo

--- src/main/java/id/ac/ui/cs/advpro/trackorder/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advpro/trackorder/repository/OrderRepository.java
@@ -2,9 +2,9 @@ package id.ac.ui.cs.advpro.trackorder.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import id.ac.ui.cs.advpro.trackorder.models.order;
+import id.ac.ui.cs.advpro.trackorder.models.Order;
 
 @Repository
-public interface OrderRepository extends JpaRepository<order, Long>{
+public interface OrderRepository extends JpaRepository<Order, Long>{
     // ini kosong karena inheritnya dari JpaRepository yang sudah ada method-methodnya
 }
