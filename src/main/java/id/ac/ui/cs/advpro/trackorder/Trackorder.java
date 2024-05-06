664a9ed38f3a422a913b4c51d363e0855b3daa6b
Oey Joshua Jodrian <joshuajodrian@gmail.com>
2024-05-06 10:44:06 +0700

    [REFACTOR] Fix dependencies

--- src/main/java/id/ac/ui/cs/advpro/trackorder/Trackorder.java
+++ src/main/java/id/ac/ui/cs/advpro/trackorder/Trackorder.java
@@ -1,19 +1,24 @@
 package id.ac.ui.cs.advpro.trackorder;
 
+import id.ac.ui.cs.advpro.trackorder.order.Order;
+import id.ac.ui.cs.advpro.trackorder.order.OrderObserver;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Trackorder implements OrderObserver {
     private List<Order> orders = new ArrayList<>();
+    public String orderId;
 
     public Trackorder(String orderId, Order order) {
         this.orderId = orderId;
         order.addObserver(this);
     }
 
+
     @Override
-    public void update(String status) {
-        //TODO: Isi
+    public void update(Order order) {
+
     }
 
     @Override
