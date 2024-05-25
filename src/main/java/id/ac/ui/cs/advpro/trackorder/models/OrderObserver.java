e27efa11cfd22e2c2195b26c1f269bf9d616ab1a
Oey Joshua Jodrian <joshuajodrian@gmail.com>
2024-05-26 01:10:16 +0700

    [REFACTOR] change design pattern

--- src/main/java/id/ac/ui/cs/advpro/trackorder/models/OrderObserver.java
+++
@@ -1,7 +0,0 @@
-package id.ac.ui.cs.advpro.trackorder.models;
-
-public interface OrderObserver {
-    void update(Order order);
-    void addOrder(Order order);
-    void removeOrder(Order order);
-}
