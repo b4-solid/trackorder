32b9ba4b5ba0836a76074b4345fd2e01f7a68416
Oey Joshua Jodrian <joshuajodrian@gmail.com>
2024-05-26 01:15:16 +0700

    [RED] Create seeding service skeleton

--- src/main/java/id/ac/ui/cs/advpro/trackorder/service/SeedingService.java
+++ src/main/java/id/ac/ui/cs/advpro/trackorder/service/SeedingService.java
@@ -1,2 +1,19 @@
-package id.ac.ui.cs.advpro.trackorder.service;public class SeedingService {
+package id.ac.ui.cs.advpro.trackorder.service;
+
+import id.ac.ui.cs.advpro.trackorder.models.Order;
+import id.ac.ui.cs.advpro.trackorder.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SeedingService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public void seedOrders() {
+        // Method implementation
+    }
 }
