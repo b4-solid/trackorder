c50056be24c872001dbd3eb8ea0e2d07f30604b6
Oey Joshua Jodrian <joshuajodrian@gmail.com>
2024-05-26 01:11:37 +0700

    [RED]Create seeding controller skeleton

--- src/main/java/id/ac/ui/cs/advpro/trackorder/controller/SeedingController.java
+++ src/main/java/id/ac/ui/cs/advpro/trackorder/controller/SeedingController.java
@@ -1,2 +1,22 @@
-package id.ac.ui.cs.advpro.trackorder.controller;public class SeedingController {
+package id.ac.ui.cs.advpro.trackorder.controller;
+
+import id.ac.ui.cs.advpro.trackorder.service.SeedingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/seed")
+public class SeedingController {
+
+    @Autowired
+    private SeedingService orderSeedingService;
+
+    @GetMapping("/orders")
+    public ResponseEntity<String> seedOrders() {
+        // Method implementation
+        return null;
+    }
 }
